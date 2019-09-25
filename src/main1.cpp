1a45116b462aa6ba35c1556b924b14c5ee983d5b
Олеся Мартынюк <olesyamartinyuk@gmail.com>
2019-09-25 14:11:17 +0300

    B58 Martynyuk 18PI-2 Task1

---
+++ src/main1.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SoftwareLab1
+//
+//  Created by Олеся Мартынюк on 25/09/2019.
+//  Copyright © 2019 Olesia Martinyuk. All rights reserved.
+//
+
+#include <iostream>
+#include "task1.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    unsigned long value;
+    value = findValue(1,20);
+    cout<<"The minimum number is "<<value<<endl;
+}
