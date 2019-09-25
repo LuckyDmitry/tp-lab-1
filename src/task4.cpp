2e889ddedd033949ae6006816071709a8c15922f
Windows 7 <student7@std.nnov.corp.local>
2019-09-25 15:36:47 +0300

    B52 SDanilov Task 4

---
+++ src/task4.cpp
@@ -0,0 +1,64 @@
+#include "task4.h"
+char * sum(char *x, char *y){
+	#include <stdlib.h>
+int main(){
+	char *x = "312\0";
+	char *y = "12\0";
+	if (!x || !y){
+		return 0;
+	}
+	char *i = x;
+	char *j = y;
+	auto sizex = 0ULL;
+	auto sizey = 0ULL;
+	auto sizeres = 0ULL;
+	while (*(i) != '\0'){
+		i++;
+		sizex++;
+	}
+	while (*(j) != '\0'){
+		j++;
+		sizey++;
+	}
+	i--;
+	j--;
+	char * result;
+	if (sizex > sizey){
+		sizeres = sizex;
+	}
+	else{
+		sizeres = sizey;
+	}
+	result = (char*)malloc(sizeres + 1);
+	char* ires = result + sizeres;
+	int inc = 0;
+	int s = 0;
+	while (i != x-1 && j != y-1){
+		s = (*(i)-'0') + (*(j)-'0');
+		if (s >= 10){
+			s -= 10;
+			inc = 1;
+		}
+		if (inc == 1){
+			inc = 0;
+			s += 1;
+		}
+		*(ires) = s + '0';
+		i--;
+		j--;
+		ires--;
+	}
+	while (i != x - 1){		
+		*(ires) = *(i);
+		i--;
+		ires--;
+	}
+	while (j != y - 1){
+		*(ires) = *(j)+'0';
+		j--;
+		ires--;
+	}
+	if (inc == 1){
+		*(ires) = 1;
+	}
+}
