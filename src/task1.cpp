cc8172dbf2479fa025eb74e00fc85c9feb839fcf
Windows 7 <student7@std.nnov.corp.local>
2019-09-25 14:25:50 +0300

    B52 SDanilov Task1

---
+++ src/task1.cpp
@@ -0,0 +1,18 @@
+#include "task1.h"
+unsigned long findValue(unsigned int min,unsigned max){
+	auto i = 1UL;
+	while(true){
+		is_divided = 1;
+		for (int j = min; j <= max; j++){
+			if (i%j != 0){
+				is_divided =0;
+				break;
+			}
+		}
+		if (is_divided == 1){
+			break;
+		}
+		i++;
+	}
+	return i;
+}
