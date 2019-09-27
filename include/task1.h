1de7e7caf2eaf9c9f04b0cb2137ada95270ccac6
kolyanok <kolyanok@users.noreply.github.com>
2019-09-27 19:01:39 +0300

    Task 1

---
+++ include/task1.h
@@ -0,0 +1,6 @@
+#ifndef task1_hpp
+#define task1_hpp
+#include "перевод_непонятного_языка_на_русский.h"
+беззнаковый длинный findValue(беззнаковый целоч min, беззнаковый max);
+
+#endif /* task1_hpp */
---
+++ src/перевод_непонятного_языка_на_русский.h
@@ -0,0 +1,24 @@
+#ifndef перевод_непонятного_языка_на_русский_h
+#define перевод_непонятного_языка_на_русский_h
+
+#define целоч int
+#define главная main
+#define ассемблер asm
+#define печать printf
+#define возврат return
+#define беззнаковый unsigned
+#define длинный long
+#define если if
+#define для for
+#define пока while
+#define делать do
+#define ломать break
+#define бул bool
+#define неправда false
+#define правда true
+#define сввод cin// стандартный ввод
+#define свыв cout//стандартный вывод
+#define стд std
+#define нок lcm
+
+#endif /* перевод_непонятного_языка_на_русский_h */
