8d0a335fa0e2d7671dd4b679ec1d4773c4368027
Valerii <valerider@icloud.com>
2019-09-29 18:21:24 +0300

    5 fixed again

--- src/task5.cpp
+++ src/task5.cpp
@@ -10,12 +10,15 @@ void split(char ***result, int *N, char *buf, char ch) {
 	i = 0;
 	*N = num + 1;
 	*result = new char*[num + 1];
-	for (int j = 0; j < num + 1; j++) {
-		num = 0;
+	int dop = num + 1;
+	for (int j = 0; j < dop; j++) {
+		/*num = 0;
 		while (*(buf + i) != '\0' && *(buf + i) != ch) {
 			i++;
 			num++;
-		}
+		}*/
+		num = 3;
+		i = 3;
 		*(*result + j) = new char[num + 1];
 		*(*(*result + j)+num) = '\0';
 		for (int k = 0; k < num; k++) {
