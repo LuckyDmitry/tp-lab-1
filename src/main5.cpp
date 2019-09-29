a2efc637dfc00eac8886246cf0ca58cea337af4d
Valerii <valerider@icloud.com>
2019-09-29 14:18:34 +0300

    task5 fixed

--- src/main5.cpp
+++ src/main5.cpp
@@ -7,12 +7,15 @@ using namespace std;
 int main() {
 	char buf[20] = { 0 }, ch;//"123,456,789";
 	int N = 0, j = 0;
-	char **result = nullptr;
+	char **result = NULL;
 	cin >> buf;
 	cin >> ch;
 	split(&result, &N, buf, ch);
-	cout << result[0] << "\n" << result[1] << "\n" << result[2] << "\n";
-	
+	for (int i = 0; i < N; i++) {
+		cout << result[i] << "\n";
+		delete[] result[i];
+	}
+
 	system("pause");
 	return 0;
 }
