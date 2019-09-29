6241d869c6f2421628dc03a0cba3297b286ceba8
Valerii <valerider@icloud.com>
2019-09-29 13:43:08 +0300

    5

--- src/main5.cpp
+++ src/main5.cpp
@@ -5,19 +5,14 @@
 #include "task5.h"
 using namespace std;
 int main() {
-	char buf[] = "123,456,789";
+	char buf[20] = { 0 }, ch;//"123,456,789";
 	int N = 0, j = 0;
 	char **result = nullptr;
-	split(&result, &N, buf, ',');
-
-	for (int i = 0; i < N; i++) {
-		j = 0;
-		while (*(*(result + i) + j) != '\0') {
-			cout << *(*(result + i) + j);
-			j++;
-		}
-		cout << "\n";
-	}
+	cin >> buf;
+	cin >> ch;
+	split(&result, &N, buf, ch);
+	cout << result[0] << "\n" << result[1] << "\n" << result[2] << "\n";
+	
 	system("pause");
 	return 0;
 }
