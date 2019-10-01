1556b5cc16b612857dc9bc743552b7e1d8275575
aailyin <artyomgoodguy123@gmail.com>
2019-10-01 19:04:51 +0300

    Update main5.cpp

--- src/main5.cpp
+++ src/main5.cpp
@@ -7,8 +7,7 @@ int main() {
 	char buf[] = "avsc/2weq/123";
 	int N = 0, i = 0;
 	char **result = nullptr;
-	char ch = '/';
-	split(&result, &N, buf, ch);
+	split(&result, &N, buf, '/');
 	for (i = 0; i < N; i++) {
 		std::cout << result[i] << "\n";
 	}
