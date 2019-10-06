373da26a58484994d2811b0bb3a03cfd2deaca77
Dasha <wqe>
2019-10-06 13:10:37 +0300

    fixed task3.cpp

--- src/task3.cpp
+++ src/task3.cpp
@@ -4,11 +4,23 @@
 
 #include "task3.h"
 #include "task2.h"
-unsigned long long sumPrime(unsigned int hbound){
-    unsigned long long sum=0;
-    int max=hbound;
-    for(int i=2;i<max;i++){
-        if(checkPrime(i)==true)sum+=i;
+
+unsigned long long sumPrime(unsigned int hbound)
+{
+    unsigned long long summ = 0;
+    bool* tmp = new bool[hbound];
+    for (int i = 0; i < hbound; i++){
+        tmp[i] = true;
     }
-    return sum;
+    for (int i = 2; i * i <= hbound; i++){
+        if (tmp[i])
+            for (int j = i * i; j <= hbound; j += i){
+                tmp[j] = false;
+            }
+    }
+    for (int i = 2; i < hbound; i++){
+        if (tmp[i]) summ += i;
+    }
+    return summ;
 }
+
