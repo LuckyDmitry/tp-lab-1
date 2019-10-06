8f99f3c011bb6beb812ad8fd5a2391923b88b425
Pavel <zamelinpavel@gmail.comgit config --global user.email zamelinpavel@gmail.comgit config --global user.email zamelinpavel@gmail.com>
2019-10-06 12:49:18 +0300

    bloody fucking try

---
+++ src/main4.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdlib.h>
+#include "task4.h"
+
+
+int main()
+{
+	char s1[] = "99999999999999999999";
+	char s2[] = "1";
+	std::cout << ""<<sum(s1,s2);
+
+	return 0;
+}
