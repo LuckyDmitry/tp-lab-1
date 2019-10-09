b2415846165816722bb96ffe66691008fb9c547e
Andrei Zhuravlev <dwasdx@gmail.com>
2019-10-09 12:58:08 +0300

    added library

--- src/task5.cpp
+++ src/task5.cpp
@@ -10,6 +10,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 void split(char ***result, int *N, char *buf, char ch) {
@@ -34,3 +35,34 @@ void split(char ***result, int *N, char *buf, char ch) {
     int a = 1;
     return;
 }
+
+//void split(char ***substringArray, int *n, char *buf, char separator)
+//{
+//   std::vector<char> *currentSubstring = new std::vector<char>();
+//   std::vector<char *> *substrings = new std::vector<char *>();
+//
+//   for (const char *stringIterator = buf; stringIterator - buf <= (int32_t)strlen(buf); stringIterator++)
+//   {
+//       if ((*stringIterator == separator || *stringIterator == '\0') && currentSubstring->size() > 0)
+//       {
+//           uint32_t currentSubstringArrayLength = currentSubstring->size();
+//           char *currentSubstringArray = new char[currentSubstringArrayLength + 1];
+//           for (uint32_t i = 0; i < currentSubstringArrayLength; i++)
+//               currentSubstringArray[i] = (*currentSubstring)[i];
+//           currentSubstringArray[currentSubstringArrayLength] = '\0';
+//
+//           currentSubstring->clear();
+//           substrings->push_back(currentSubstringArray);
+//       }
+//       else if (*stringIterator != separator)
+//           currentSubstring->push_back(*stringIterator);
+//   }
+//
+//   *n = substrings->size();
+//   *substringArray = new char *[*n];
+//   for (uint32_t i = 0; i < *n; i++)
+//       (*substringArray)[i] = (*substrings)[i];
+//
+//   delete currentSubstring;
+//   delete substrings;
+//};
