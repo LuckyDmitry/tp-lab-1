bb4df3cf47439ccfd5bf861cca816ec751d755f8
Andrei Zhuravlev <dwasdx@gmail.com>
2019-10-10 11:27:03 +0300

    hope, everrything works fine

--- src/task2.cpp
+++ src/task2.cpp
@@ -8,8 +8,10 @@
 
 #include "task2.h"
 #include <cmath>
-
+//fixed
 bool checkPrime(unsigned int value) {
+    if (value == 2)
+        return true;
     if (value % 2 == 0 || value == 1)
         return false;
     for (int i = 3; i <= sqrt(value); i += 2) {
@@ -25,23 +27,29 @@ unsigned long long nPrime(unsigned n) {
         return 2;
     if (n == 2)
         return 3;
-    int i = 2;
+    n--;
     unsigned long long num = 1;
-    while (i != n) {
-        if (checkPrime(num))
-            i++;
+    while (n > 0) {
         num += 2;
+        if (checkPrime(num))
+            n--;
     }
     return num;
 }
 
 unsigned long long nextPrime(unsigned long long value) {
-    if (value == 1)
+    if (value == 1 || value == 0)
         return 2;
     if (value == 2)     //Чтобы не было проблем с четными числами
         return 3;
-    unsigned long long num = value + 2;
-    while (!checkPrime(num))
-        num += 2;
-    return num;
+//    unsigned long long num = value + 2;
+//    while (!checkPrime(num))
+//        num += 2;
+//    return num;
+    if (value % 2 == 0)
+        value += 3;
+    value += 2;
+    while (!checkPrime(value))
+        value += 2;
+    return value;
 }
