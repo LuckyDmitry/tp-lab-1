8f02be29496f44aabf661821a923d5b3fa3de72b
kolyanok <kolyanok@users.noreply.github.com>
2019-10-13 19:38:07 +0300

    Translation

--- include/перевод_непонятного_языка_на_русский.h
+++ include/перевод_непонятного_языка_на_русский.h
@@ -1,3 +1,11 @@
+//
+//  перевод_непонятного_языка_на_русский.h
+//  лабораторная работа
+//
+//  Created by Коля on 25/09/2019.
+//  Copyright © 2019 Коля. All rights reserved.
+//
+
 #ifndef перевод_непонятного_языка_на_русский_h
 #define перевод_непонятного_языка_на_русский_h
 
@@ -20,6 +28,26 @@
 #define свыв cout//стандартный вывод
 #define стд std
 #define нок lcm
+#define пустой void
+#define обещ promise
+#define задать_значение set_value
+#define взять_будущее get_future
+#define взять get
+#define поток thread
+#define авто auto
+#define перемещ move
+#define присоединить join
+#define симв char
+#define строка string
+#define разм size
+#define обмен swap
+#define си_стр c_str
+#define стрдуп strdup
+#define размер sizeof
+#define памназ malloc
+#define стрдлин strlen
+#define истрокпоток istringstream
+#define взятьстроку getline
 #define нулевуказ nullptr
 
 #endif /* перевод_непонятного_языка_на_русский_h */
