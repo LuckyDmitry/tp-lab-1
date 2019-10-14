41a13501ed4ce53d92b454e0321f57fb9d1d94c5
Aleksandr Komyagin <sashawqw@mail.ru>
2019-10-14 11:26:36 +0300

    last 56

--- src/task3.cpp
+++ src/task3.cpp
@@ -1,20 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "task3.h"
-
-
-bool checkPrime(unsigned int value)
-{
-	int i;
-	if (value < 2)
-		return false;
-	for (int i = 2; i*i <= value; i++)
-	{
-		if (value % i == 0)
-			return false;
-	}
-	return true;
-}
-
+#include "task2.h"
 
 
 unsigned long long sumPrime(unsigned int hbound)
