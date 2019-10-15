e4cb24e0ba7950090236bac8d522268fa599512b
Ekaterina <55396312+BogoroditskayaEkaterina@users.noreply.github.com>
2019-10-15 14:08:52 +0300

    Update main2.cpp

--- src/main2.cpp
+++ src/main2.cpp
@@ -4,8 +4,8 @@
 
 int main()
 {
-	printf("%d \n", checkPrime(11));
-	printf("%d \n", nPrime(11));
-	printf("%d \n", nextPrime(11));
-	return 1;
+	printf("%d \n", checkPrime(12));
+	printf("%d \n", nPrime(500));
+	printf("%d \n", nextPrime(2));
+	return 0;
 } 
