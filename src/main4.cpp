f752191ea02cacfcd4d40e1aca1cb16fc1f827aa
Ekaterina <55396312+BogoroditskayaEkaterina@users.noreply.github.com>
2019-10-15 13:16:04 +0300

    Update main4.cpp

--- src/main4.cpp
+++ src/main4.cpp
@@ -1,11 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
 #include "task4.h"
+#include <iostream>
 
 int main()
 {
-	const char x[] = { "123000" };
-	const char y[] = { "456123" };
+	char x[] = { "123000" };
+	char y[] = { "456123" };
 
 	printf("\n%s", sum(x, y));
 	return 1;
