29e7ff05ae78adc891d1bd526d78df13d01e3011
Ekaterina <55396312+BogoroditskayaEkaterina@users.noreply.github.com>
2019-10-22 23:32:24 +0300

    Update main5.cpp

--- src/main5.cpp
+++ src/main5.cpp
@@ -1,16 +1,16 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include "task5.h"
 #include <iostream>
 
-int main() {
-	char *buf = "123,456,789";
+int main() 
+{
+	char buf[] = "You are having very good time";
 	int N = 0;
-	char **result = nullptr;
-	split(&result, &N, buf, ',');
+	char **result = NULL;
+
+	split(&result, &N, buf, ' ');
 	for (int i = 0; i < N; i++)
 	{
 		puts(*(result + i));
 	}
-	return 0;
+	return 1;
 }
