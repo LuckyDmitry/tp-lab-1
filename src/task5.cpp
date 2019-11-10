81157650b59a5ee6266a52dbf8ff11c86772a288
unknown <nastenckavoronowa@yandex.ru>
2019-11-10 23:34:38 +0300

    second commit

--- src/task5.cpp
+++ src/task5.cpp
@@ -3,22 +3,23 @@
 using namespace std;
 void split(char*** result, int* N, char* buf, char ch)
 {
-	int n = strlen(buf);
-	for (int i = 0; i < n; i++)
+	int i = 0;
+	while(bif[i]!='\0')
 	{
 		if (buf[i] == ch)
 			(*N)++;
+		i++;
 	}
 	(*N)++;
 
-
+	i = 0;
 
 	*result = new char* [*N];
 	char* check = new char[n];
 	*(*result) = check;
 
 	int k = 1;
-	for (int i = 0; i < n; i++)
+	while (bif[i] != '\0')
 	{
 		check[i] = buf[i];
 		if (buf[i] == ch)
@@ -28,8 +29,10 @@ void split(char*** result, int* N, char* buf, char ch)
 			check[i] = '\0';
 
 		}
-		if (i == n - 1) check[i + 1] = '\0';
+		i++;
+		
 	}
+	check[i] = '\0';
 
 
 
