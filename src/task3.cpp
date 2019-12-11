d4499f61f5809be236c3461f48cf4a025d1a785a
Sergey <baranenkovs@mail.ru>
2019-12-11 11:51:20 +0300

    Исправлен баг в 3 задании (вроде)

--- src/task3.cpp
+++ src/task3.cpp
@@ -1,22 +1,30 @@
 #include "task3.h"
+#include <iostream>
 #include <math.h>
 unsigned long long sumPrime(unsigned int hbound) //20
 {
     if (hbound<3)return 0;
     unsigned long long i, j, sum = 2;//sum=2 тк в данной реализации алгоритма 2*i+1 не учитывает четные числа (2-простое число)
     hbound=round((hbound)/2);
-    char a[hbound];
-
+    auto a = new bool[hbound];
     for (i=1; i<hbound; i++)
-        a[i]=1;
+        a[i]=true;
 
 
-    for(i=1; 2*i*(i+1)<hbound; i++)
-        for(j=i; j<=(hbound-i)/(2*i+1); j++)
-            a[2*i*j+i+j]=0;
-
-    for(i=0; i<hbound; i++)
+    i=1; j=1;
+    while ((2*i*j+i+j)<=hbound)
+    {
+        while (j<=(hbound-i)/(2*i+1))
+        {
+            a[2*i*j+i+j]=false;
+            j++;
+        }
+        i++;
+        j=i;
+    }
+    for(i=1; i<hbound; i++)
         if(a[i])
             sum+=(2*i+1);
+    delete []a;
     return sum;
 }
